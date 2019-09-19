6369aa86e28e013a78b0fc7a44c1814fbe0c7fc3
hyeddo <syong0716@naver.com>
2019-09-19 20:46:50 +0900

    hyeddo_Bj

---
+++ 1065.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int input, count=0;
+	cin >> input;
+	if (input > 0 && input <= 1000) {
+		for (int i = 1; i <= input; i++) {
+			if (i < 100) {
+				count++; //일의자리수는 다 카운트
+			}
+			else if (i < 1000) {
+				if ((i % 10) - ((i / 10) % 10) == ((i / 10) % 10) - (i / 100)) count++;
+			}
+		}
+	}
+	else cout << "범위 안의 수를 입력하세요" << endl;
+
+	cout << count;
+}
