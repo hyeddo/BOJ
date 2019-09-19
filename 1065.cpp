7703c62f18a45db4ff5947bb698a53b58e3024d4
hyeddo <syong0716@naver.com>
2019-09-19 21:16:53 +0900

    done

--- 1065.cpp
+++ 1065.cpp
@@ -2,19 +2,15 @@
 using namespace std;
 
 int main() {
-	int input, count=0;
+	int input, cnt=0;
 	cin >> input;
-	if (input > 0 && input <= 1000) {
-		for (int i = 1; i <= input; i++) {
-			if (i < 100) {
-				count++; //한자리수, 두자리수는 다 카운트
-			}
-			else if (i < 1000) {
-				if ((i % 10) - ((i / 10) % 10) == ((i / 10) % 10) - (i / 100)) count++;
-			}
+	for (int i = 1; i <= input; i++) {
+		if (i < 100) {
+			cnt++; //한자리수, 두자리수는 다 카운트
+		}
+		else if (i < 1000) {
+			if ((i % 10) - ((i / 10) % 10) == ((i / 10) % 10) - (i / 100)) cnt++;
 		}
 	}
-	else cout << "범위 안의 수를 입력하세요" << endl;
-
-	cout << count;
+	cout << cnt;
 }
