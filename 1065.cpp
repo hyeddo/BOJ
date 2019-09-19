7a3282a93374fa5b509f47aaa45e87cdebb422e8
hyeddo <syong0716@naver.com>
2019-09-19 20:51:18 +0900

    hyeddo_Bj

--- 1065.cpp
+++ 1065.cpp
@@ -7,7 +7,7 @@ int main() {
 	if (input > 0 && input <= 1000) {
 		for (int i = 1; i <= input; i++) {
 			if (i < 100) {
-				count++; //일의자리수는 다 카운트
+				count++; //한자리수, 두자리수는 다 카운트
 			}
 			else if (i < 1000) {
 				if ((i % 10) - ((i / 10) % 10) == ((i / 10) % 10) - (i / 100)) count++;
