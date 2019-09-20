8792eac3873a22e99a0c04932d1b6dcab155df19
hyeddo <syong0716@naver.com>
2019-09-20 19:07:05 +0900

    done

---
+++ 10952_A+B-5_0920.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int Sum(int A, int B) {
+	int res = A + B;
+	return res;
+}
+
+int main(){
+	int A, B;
+	while (1) {
+		cin >> A >> B;
+		if (A == 0 && B == 0) break;
+		cout << Sum(A, B) << '\n';
+	}
+	return 0;	
+}
