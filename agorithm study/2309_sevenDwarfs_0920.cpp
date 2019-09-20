c86fd1c79a50f427795dcd2e472abbba3f39bd93
hyeddo <syong0716@naver.com>
2019-09-20 19:22:27 +0900

    done

--- agorithm study/2309_sevenDwarfs_0920.cpp
+++ agorithm study/2309_sevenDwarfs_0920.cpp
@@ -11,17 +11,17 @@ int main() {
 
 	falseSum = sum - 100;
 
-	for (int j = 0; j < 9; j++) {
-		for (int k = j + 1; k < 9; k++) {
-			if ((dwarf[j] + dwarf[k]) == falseSum)
+	for (int i = 0; i < 9; i++) {
+		for (int j = i + 1; j < 9; j++) {
+			if ((dwarf[i] + dwarf[j]) == falseSum)
 			{
+				dwarf[i] = -1;
 				dwarf[j] = -1;
-				dwarf[k] = -1;
 
 				sort(dwarf, dwarf + 9);
 
-				for (int m = 2; m < 9; m++)
-					cout << dwarf[m] << '\n';
+				for (int k= 2; k < 9; k++)
+					cout << dwarf[k] << '\n';
 
 				return 0;
 			}
