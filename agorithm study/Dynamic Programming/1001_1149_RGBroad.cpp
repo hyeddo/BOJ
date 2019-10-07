2b62c536243d4cb227fae6e8ce9034340bc917ef
hyeddo <syong0716@naver.com>
2019-10-07 15:37:53 +0900

    done

---
+++ agorithm study/Dynamic Programming/1001_1149_RGBroad.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define MAX 1001
+
+int N, minPrice;
+int RGB[MAX][3];
+int cache[MAX][3];
+
+int main() {
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		cin >> RGB[i][0] >> RGB[i][1] >> RGB[i][2];
+	}
+
+	for (int i = 0; i < N; i++) {
+		cache[i][0] = min(cache[i - 1][1], cache[i - 1][2]) + RGB[i][0];
+		cache[i][1] = min(cache[i - 1][0], cache[i - 1][2]) + RGB[i][1];
+		cache[i][2] = min(cache[i - 1][0], cache[i - 1][1]) + RGB[i][2];
+	}
+
+	minPrice = min(min(cache[N - 1][0], cache[N - 1][1]), cache[N - 1][2]);
+
+	cout << minPrice << '\n';
+}
